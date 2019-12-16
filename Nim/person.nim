b2bee72ee598fa408154887a47d3d7f4a1274905
Andrew Shitov <andy@shitov.ru>
2019-12-16 17:04:21 +0100

    Fix

--- Nim/person.nim
+++ Nim/person.nim
@@ -3,7 +3,7 @@ type
     name: string
     age: int
     
-proc info(o: var Person): string =
+proc info(o: Person): string =
   o.name & " is " & $o.age & " years old"
 
 var p = Person(name: "John", age: 40)
